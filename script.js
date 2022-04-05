93d7fd4c7b84b6a8b0714866ccf94811ab316a7f
yosefHesham <yousefmobdev@gmail.com>
2022-04-05 12:32:11 +0200

    Style nav items

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let navButton = document.querySelector("#mob-menu");
+let hamburger = document.querySelector("#mob-menu");
 let navBar = document.querySelector(".nav-list");
 let closeBtn = document.querySelector(".close-button")
 let navItems = document.querySelectorAll(".nav-item");
@@ -8,21 +8,21 @@ var isNavVisible = false
 function onClick() {
   if(isNavVisible) {
     navBar.style.display = "none";
-    navButton.style.display = "block";
+    hamburger.style.display = "block";
     closeBtn.style.display = "none";
     isNavVisible = false;
   }
   
   else {
     navBar.style.display = "block"
-    navButton.style.display = "none";
+    hamburger.style.display = "none";
     closeBtn.style.display = "block";
     isNavVisible = true;
   }
  
 }
+hamburger.addEventListener("click" , onClick);
 closeBtn.addEventListener("click",onClick)
-navButton.addEventListener("click" , onClick);
 navItems.forEach((e) => {
   e.addEventListener("click",onClick)
 });
