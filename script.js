053769218a12fc4b3310ffafc22101498b90627e
yosefHesham <yousefmobdev@gmail.com>
2022-04-05 11:42:49 +0200

    Change list-tyle to none

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 let navButton = document.querySelector("#mob-menu");
 let navBar = document.querySelector(".nav-list");
+let closeBtn = document.querySelector(".close-button")
 var isNavVisible = false
 
 
 function onClick() {
-  console.log(isNavVisible);
   if(isNavVisible) {
     navBar.style.display = "none";
     navButton.style.display = "block";
+    closeBtn.style.display = "none";
     isNavVisible = false;
   }
   
   else {
     navBar.style.display = "block"
     navButton.style.display = "none";
+    closeBtn.style.display = "block";
     isNavVisible = true;
   }
  
 }
+closeBtn.addEventListener("click",onClick)
 navButton.addEventListener("click" , onClick);
