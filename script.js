eb77cadd7647a2f6322c218ba8e0f382f2b88502
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-04-05 17:16:50 +0200

    Add Padding to the nav  mob-list and fix linter errors

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-let hamburger = document.querySelector("#mob-menu");
-let navBar = document.querySelector(".mob-nav-list");
-let closeBtn = document.querySelector(".close-button")
-let navItems = document.querySelectorAll(".nav-item");
-var isNavVisible = false
-
+const hamburger = document.querySelector('#mob-menu');
+const navBar = document.querySelector('.mob-nav-list');
+const closeBtn = document.querySelector('.close-button');
+const navItems = document.querySelectorAll('.nav-item');
+let isNavVisible = false;
 
 function onClick() {
-  if(isNavVisible) {
-    navBar.style.display = "none";
-    hamburger.style.display = "block";
-    closeBtn.style.display = "none";
+  if (isNavVisible) {
+    navBar.style.display = 'none';
+    hamburger.style.display = 'block';
+    closeBtn.style.display = 'none';
     isNavVisible = false;
-  }
-  
-  else {
-    navBar.style.display = "block"
-    hamburger.style.display = "none";
-    closeBtn.style.display = "block";
+  } else {
+    navBar.style.display = 'block';
+    hamburger.style.display = 'none';
+    closeBtn.style.display = 'block';
     isNavVisible = true;
   }
- 
 }
-hamburger.addEventListener("click" , onClick);
-closeBtn.addEventListener("click",onClick)
+hamburger.addEventListener('click', onClick);
+closeBtn.addEventListener('click', onClick);
 navItems.forEach((e) => {
-  e.addEventListener("click",onClick)
+  e.addEventListener('click', onClick);
 });
