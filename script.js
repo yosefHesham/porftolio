b0fde7cceb410b0a2119c4ca9848a65cc132379f
yosefHesham <yousefmobdev@gmail.com>
2022-04-06 15:16:00 +0200

    Create array data

--- script.js
+++ script.js
@@ -1,24 +1,123 @@
-const hamburger = document.querySelector('#mob-menu');
-const navBar = document.querySelector('.mob-nav-list');
-const closeBtn = document.querySelector('.close-button');
-const navItems = document.querySelectorAll('.nav-item');
+const hamburger = document.querySelector("#mob-menu");
+const navBar = document.querySelector(".mob-nav-list");
+const closeBtn = document.querySelector(".close-button");
+const navItems = document.querySelectorAll(".nav-item");
 let isNavVisible = false;
 
 function onClick() {
   if (isNavVisible) {
-    navBar.style.display = 'none';
-    hamburger.style.display = 'block';
-    closeBtn.style.display = 'none';
+    navBar.style.display = "none";
+    hamburger.style.display = "block";
+    closeBtn.style.display = "none";
     isNavVisible = false;
   } else {
-    navBar.style.display = 'block';
-    hamburger.style.display = 'none';
-    closeBtn.style.display = 'block';
+    navBar.style.display = "block";
+    hamburger.style.display = "none";
+    closeBtn.style.display = "block";
     isNavVisible = true;
   }
 }
-hamburger.addEventListener('click', onClick);
-closeBtn.addEventListener('click', onClick);
+hamburger.addEventListener("click", onClick);
+closeBtn.addEventListener("click", onClick);
 navItems.forEach((e) => {
-  e.addEventListener('click', onClick);
+  e.addEventListener("click", onClick);
 });
+
+const projects = [
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    technologies: ["Ruby on rails, css, Javascript ,html"],
+    desktopPopupTech: [
+      "Codekit",
+      "Github",
+      "Javascript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    mobilePopupTech: ["Ruby on rails,  css,javascript"],
+    title: "Keeping track of hundreds of components",
+    popUpImageDesktop: "assets/images/snapshot-desktop.png",
+    popUpImageMobile: "assets/images/snapshoot-portfolio.png",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    technologies: ["Ruby on rails, css, Javascript ,html"],
+    desktopPopupTech: [
+      "Codekit",
+      "Github",
+      "Javascript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    mobilePopupTech: ["Ruby on rails,  css,javascript"],
+    title: "Keeping track of hundreds of components",
+    popUpImageDesktop: "assets/images/snapshot-desktop.png",
+    popUpImageMobile: "assets/images/snapshoot-portfolio.png",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    technologies: ["Ruby on rails, css, Javascript ,html"],
+    desktopPopupTech: [
+      "Codekit",
+      "Github",
+      "Javascript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    mobilePopupTech: ["Ruby on rails,  css,javascript"],
+    title: "Keeping track of hundreds of components",
+    popUpImageDesktop: "assets/images/snapshot-desktop.png",
+    popUpImageMobile: "assets/images/snapshoot-portfolio.png",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    technologies: ["Ruby on rails, css, Javascript ,html"],
+    desktopPopupTech: [
+      "Codekit",
+      "Github",
+      "Javascript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    mobilePopupTech: ["Ruby on rails,  css,javascript"],
+    title: "Keeping track of hundreds of components",
+    popUpImageDesktop: "assets/images/snapshot-desktop.png",
+    popUpImageMobile: "assets/images/snapshoot-portfolio.png",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    technologies: ["Ruby on rails, css, Javascript ,html"],
+    desktopPopupTech: [
+      "Codekit",
+      "Github",
+      "Javascript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    mobilePopupTech: ["Ruby on rails,  css,javascript"],
+    title: "Keeping track of hundreds of components",
+    popUpImageDesktop: "assets/images/snapshot-desktop.png",
+    popUpImageMobile: "assets/images/snapshoot-portfolio.png",
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    technologies: ["Ruby on rails, css, Javascript ,html"],
+    desktopPopupTech: [
+      "Codekit",
+      "Github",
+      "Javascript",
+      "Bootstrap",
+      "Terminal",
+      "Codepen",
+    ],
+    mobilePopupTech: ["Ruby on rails,  css,javascript"],
+    title: "Keeping track of hundreds of components",
+    popUpImageDesktop: "assets/images/snapshot-desktop.png",
+    popUpImageMobile: "assets/images/snapshoot-portfolio.png",
+  },
+];
