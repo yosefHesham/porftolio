d0316a0a1987ac9b7e5dddfeaf86786fe4eeae1f
yosefHesham <yousefmobdev@gmail.com>
2022-04-08 16:14:53 +0200

    Animate error message

--- js/scripts/form-validate.js
+++ js/scripts/form-validate.js
@@ -9,6 +9,7 @@ function hasUpperCase(input) {
 form.addEventListener("submit",(event)=> {
   if(hasUpperCase(form.elements["email"].value)) {
     event.preventDefault()
+    span.style.display = "block";
     span.innerHTML = "Please make sure that email is lower case"
   }
 })
