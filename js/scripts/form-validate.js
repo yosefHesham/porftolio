b41bdd16c10a45fdb40df6bb064490c15be3c488
yosefHesham <yousefmobdev@gmail.com>
2022-04-08 17:22:44 +0200

    Finish persisting form data

--- js/scripts/form-validate.js
+++ js/scripts/form-validate.js
@@ -12,19 +12,25 @@ form.addEventListener('submit', (event) => {
     span.innerHTML = 'Please make sure that email is lower case';
   }
 });
+
 let formData = {
-  name:"",
-  email:"",
-  message:""
-}
-window.onload = () => {
-  if(localStorage.getItem("formData") !== null) {
-    formData = JSON.parse(localStorage.getItem("formData"))
+  name: '',
+  email: '',
+  message: '',
+};
+function fetchLocalData() {
+  if (localStorage.getItem('formData') !== null) {
+    formData = JSON.parse(localStorage.getItem('formData'));
   }
-  form.elements["name"].value = formData.name
-  form.elements["email"].value = formData.email
-  form.elements["message"].value = formData.message
-
-  
-
+  form.elements.name.value = formData.name;
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
 }
+fetchLocalData();
+form.addEventListener('input', () => {
+  formData.name = form.elements.name.value;
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
