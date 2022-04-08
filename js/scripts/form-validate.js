edd71a09e175984a50d1465ff6140ddabb3fc62f
yosefHesham <yousefmobdev@gmail.com>
2022-04-08 15:01:59 +0200

    Validate email with js

---
+++ js/scripts/form-validate.js
@@ -0,0 +1,14 @@
+
+const form = document.querySelector(".contact-form");
+let span = document.querySelector(".error-message");
+
+function hasUpperCase(input) {
+  console.log(input);
+  return input.toLowerCase() !== input;
+}
+form.addEventListener("submit",(event)=> {
+  if(hasUpperCase(form.elements["email"].value)) {
+    event.preventDefault()
+    span.innerHTML = "Please make sure that email is lower case"
+  }
+})
