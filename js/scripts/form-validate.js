b9f15769ad38d7260cbe2920761839450b57fa3d
yosefHesham <yousefmobdev@gmail.com>
2022-04-08 17:00:14 +0200

    Create fetching saved data logic

--- js/scripts/form-validate.js
+++ js/scripts/form-validate.js
@@ -4,6 +4,7 @@ const span = document.querySelector('.error-message');
 function hasUpperCase(input) {
   return input.toLowerCase() !== input;
 }
+
 form.addEventListener('submit', (event) => {
   if (hasUpperCase(form.elements.email.value)) {
     event.preventDefault();
@@ -11,3 +12,19 @@ form.addEventListener('submit', (event) => {
     span.innerHTML = 'Please make sure that email is lower case';
   }
 });
+let formData = {
+  name:"",
+  email:"",
+  message:""
+}
+window.onload = () => {
+  if(localStorage.getItem("formData") !== null) {
+    formData = JSON.parse(localStorage.getItem("formData"))
+  }
+  form.elements["name"].value = formData.name
+  form.elements["email"].value = formData.email
+  form.elements["message"].value = formData.message
+
+  
+
+}
